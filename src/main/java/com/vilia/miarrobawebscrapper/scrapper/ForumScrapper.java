07158e3d58024b256e49ba3ed2893d33f3a9bedc
Ricardo García <ricardo@ricardogarciarivera.com>
2024-05-19 21:47:18 +0200

    ForumScrapper connection and title scrapping logic
     - Includes test class for main forum title implementation
     - Tests not executed due to some unidentified error

--- src/main/java/com/vilia/miarrobawebscrapper/scrapper/ForumScrapper.java
+++ src/main/java/com/vilia/miarrobawebscrapper/scrapper/ForumScrapper.java
@@ -3,12 +3,25 @@ package com.vilia.miarrobawebscrapper.scrapper;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.vilia.miarrobawebscrapper.model.MiarrobaForum;
 import com.vilia.miarrobawebscrapper.model.MiarrobaThread;
+import com.vilia.miarrobawebscrapper.scrapper.exception.ForumScrapperException;
 import com.vilia.miarrobawebscrapper.scrapper.support.ScrapperUrlConnector;
 
 public class ForumScrapper {
+	private static Logger logger = LoggerFactory.getLogger(ForumScrapper.class);
+	
+	private static final String TITLE_XPATH = "/html/body/div[@id=\"SectionForo\"]/table[@id = \"ForoMenuIndice\"]/tbody/tr[contains(@class, \"tablaRowFirst\")]/td/div/div[@class = \"columnsContainer\"]/div/a[1]";
+	
 	private URL forumUrl;
 	private MiarrobaForum parentForum;
 	private List<MiarrobaThread> threads;
@@ -24,26 +37,54 @@ public class ForumScrapper {
 		forum.setForumUrl(forumUrl);
 	}
 	
-	public MiarrobaForum parseForum() {
+	public MiarrobaForum parseForum() throws ForumScrapperException {
 		ScrapperUrlConnector connection = connectToForum();
-		parseForumTitle();
-		parseForumThreads();
+		parseForumTitle(connection);
+		parseForumThreads(connection);
 		
 		parseThreads();
 		
 		return forum;
 	}
 
-	private ScrapperUrlConnector connectToForum() {
-		// TODO Auto-generated method stub
-		return null;
+	private ScrapperUrlConnector connectToForum() throws ForumScrapperException {
+		ScrapperUrlConnector connection = new ScrapperUrlConnector(forumUrl);
+		
+		if (!connection.isConnectionStablished()) {
+			logger.error("Connection not stablished: ", forumUrl);
+			throw new ForumScrapperException("Connection not stablished", forumUrl, "Unknown");
+		}
+		
+		return connection;
 	}
 
-	private void parseForumTitle() {
+	private void parseForumTitle(ScrapperUrlConnector connection) {
+		Document doc = connection.getDocument();
 		
+		Elements title = doc.selectXpath(TITLE_XPATH);
+		
+		if(title.size() > 0)
+			initializeInnerForumTitle(title.first());
+		else
+			initializeFirstForumTitle();
 	}
 	
-	private void parseForumThreads() {
+	private void initializeInnerForumTitle(Element titleElement) {
+		String title = titleElement.text();
+		
+		this.forum.setForumTitle(title);
+	}
+
+	private void initializeFirstForumTitle() {
+		Pattern p = Pattern.compile("http[s]?://(\\w+)\\.mforos\\.com");
+		Matcher m = p.matcher(forumUrl.toString());
+		
+		String forumTitle = m.group(1);
+		
+		this.forum.setForumTitle(forumTitle);		
+	}
+
+	private void parseForumThreads(ScrapperUrlConnector connection) {
 		
 	}
 	
--- src/test/java/com/vilia/miarrobawebscrapper/scrapper/ForumScrapperTest.java
+++ src/test/java/com/vilia/miarrobawebscrapper/scrapper/ForumScrapperTest.java
@@ -1,8 +1,48 @@
 package com.vilia.miarrobawebscrapper.scrapper;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.vilia.miarrobawebscrapper.model.MiarrobaForum;
+import com.vilia.miarrobawebscrapper.scrapper.exception.ForumScrapperException;
+
 @ExtendWith(MockitoExtension.class)
 public class ForumScrapperTest {
+	private static final String TEST_URL = "https://vilia.miarroba.com";
+	
+	private static final String TEST_FORUM_TITLE = "vilia";
+	private static final String TEST_FIRST_INNER_FORUM_TITLE = "General";
+	
+	private ForumScrapper forumScrapper;
+	
+	public ForumScrapperTest() {
+		URL testUrl = null;
+		try {
+			testUrl = new URL(TEST_URL);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		forumScrapper = new ForumScrapper(testUrl, null);
+	}
+	
+	@Test
+	public void testForumScrapper() {
+		MiarrobaForum forum = null;
+		try {
+			forum = forumScrapper.parseForum();
+		} catch (ForumScrapperException e) {
+			Assertions.fail("forumScrapper couldn't be initialized", e);
+		}
+		
+		Assertions.assertEquals(forum.getForumTitle(), TEST_FORUM_TITLE);
+		
+		//TODO: Test first inner forum title
+	}
 }
