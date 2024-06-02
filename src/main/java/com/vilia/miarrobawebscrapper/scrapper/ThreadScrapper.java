980a111e5b688a2d07ea35f994470c332a5733ab
Ricardo García <ricardo@ricardogarciarivera.com>
2024-06-02 17:07:13 +0200

    Starting Thread scrapper

--- src/main/java/com/vilia/miarrobawebscrapper/scrapper/ThreadScrapper.java
+++ src/main/java/com/vilia/miarrobawebscrapper/scrapper/ThreadScrapper.java
@@ -1,5 +1,24 @@
 package com.vilia.miarrobawebscrapper.scrapper;
 
+import java.net.URL;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vilia.miarrobawebscrapper.model.MiarrobaThread;
+
 public class ThreadScrapper {
+	private static Logger logger = LoggerFactory.getLogger(ForumScrapper.class);
+	
+	private MiarrobaThread thread;
+	
+	public ThreadScrapper(MiarrobaThread thread) {
+		this.thread = thread;
+	}
+	
+	public ThreadScrapper(URL url) {
+		this.thread = new MiarrobaThread();
+		this.thread.setThreadUrl(url);
+	}
 
 }
